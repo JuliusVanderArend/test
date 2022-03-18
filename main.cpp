0019c3f4e6148b1af7d348c92bc3be7fa3e4f08c
JuliusVanderArend <gamingpotatostudios@gmail.com>
2022-03-18 21:43:22 +0000

    test 5

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!"); //hehe you are silly :)) (I also love you)
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!"); //hehe you are silsdfsfsfsdfsfsly :)) (I also love you)
 
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -21,5 +21,5 @@ int main()
         window.display();
     }
 
-    return 0;
+    return 0;///jahfuahfiuaiufasiufiusadf
 }
